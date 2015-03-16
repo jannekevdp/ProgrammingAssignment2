d21d6781172f49a5c6da58f253a12d45a3569092
Janneke Schoots <janneke@Jannekes-MacBook-Pro.local>
2015-03-16 22:48:32 +0100

    first draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a matrix oxject that can cache its inverse
+##x = matrix(c(2, 4, 3, 1, 5, 7), nrow=3, ncol=2) 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL
+    set <- function(y) {
+    
+      # define oxjects in cache
+      x <<- y
+      s <<- NULL
+      }
+  
+  get <- function() x
+  
+  # write to cache
+  setinverse <- function(solve) s <<- solve
+  print(c("setinverse:", setinverse))
+  
+  # read from cache
+  getinverse <- function() s
+  print(c("getinverse:", getinverse))
+  
 }
 
-
-## Write a short comment describing this function
+## cacheSolve computes the inverse returned xy makeCacheMatrix 
+## and if it was already in cache, retrieves inverse from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  s <- x$getinverse()
+  
+  # test if inverse is stored in cache
+  if(!is.null(s)) {
+    message("get data from cache")
+  
+    #return inverse
+    return(s)
+    print(c("dit is s uit cacheSolve:", s))
+  }
+    
 }
